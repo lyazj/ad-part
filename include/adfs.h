c737b2c672b145d484173d937d2c920a7f482b2c
lyazj <seeson@pku.edu.cn>
2023-05-25 16:47:06 +0800

    Add adfs.

---
+++ include/adfs.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+class ADListDir {
+
+private:
+  std::string dirpath;
+  std::vector<std::string> entnames;
+
+public:
+  ADListDir(const std::string &dirpath_in = ".", int accept = DT_ALL);
+
+  static const int DT_BLK;
+  static const int DT_CHR;
+  static const int DT_DIR;
+  static const int DT_FIFO;
+  static const int DT_LNK;
+  static const int DT_REG;
+  static const int DT_SOCK;
+  static const int DT_UNKNOWN;
+  static const int DT_ALL;
+
+  std::vector<std::string> get_names() const { return entnames; }
+  std::vector<std::string> get_full_names() const;
+
+};
---
+++ src/adfs.cpp
@@ -0,0 +1,88 @@
+#include "adfs.h"
+#include <sys/types.h>
+#include <dirent.h>
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdexcept>
+#include <string>
+#include <memory>
+#include <vector>
+#include <new>
+
+using namespace std;
+
+namespace {
+
+constexpr int DT_MAP(unsigned char d_type)
+{
+  switch(d_type) {
+    case DT_BLK: return ADListDir::DT_BLK;
+    case DT_CHR: return ADListDir::DT_CHR;
+    case DT_DIR: return ADListDir::DT_DIR;
+    case DT_FIFO: return ADListDir::DT_FIFO;
+    case DT_LNK: return ADListDir::DT_LNK;
+    case DT_REG: return ADListDir::DT_REG;
+    case DT_SOCK: return ADListDir::DT_SOCK;
+    case DT_UNKNOWN: return ADListDir::DT_UNKNOWN;
+  }
+  throw runtime_error("unknown d_type: " + to_string(d_type));
+}
+
+}  // namespace
+
+const int ADListDir::DT_BLK     =   1;
+const int ADListDir::DT_CHR     =   2;
+const int ADListDir::DT_DIR     =   4;
+const int ADListDir::DT_FIFO    =   8;
+const int ADListDir::DT_LNK     =  16;
+const int ADListDir::DT_REG     =  32;
+const int ADListDir::DT_SOCK    =  64;
+const int ADListDir::DT_UNKNOWN = 128;
+const int ADListDir::DT_ALL     = 255;
+
+ADListDir::ADListDir(const string &dirpath_in, int accept)
+{
+  if(dirpath_in.empty()) {
+    throw invalid_argument("empty dirpath");
+  }
+
+  DIR *dir = opendir(dirpath_in.c_str());
+  if(dir == NULL) {
+    throw runtime_error("opendir: "s +
+        dirpath_in + ": "s + strerror(errno));
+  }
+  shared_ptr<DIR> dir_guard(dir, [](DIR *d) { closedir(d); });
+
+  vector<string> names;
+  for(;;) {
+    errno = 0;
+    struct dirent *ent = readdir(dir);
+    if(ent == NULL) {
+      if(errno == 0) break;
+      throw runtime_error("readdir: "s +
+          dirpath_in + ": "s + strerror(errno));
+    }
+    if(DT_MAP(ent->d_type) & accept) {
+      names.push_back(ent->d_name);
+    }
+  }
+
+  dirpath = dirpath_in;
+  entnames = move(names);
+}
+
+vector<string> ADListDir::get_full_names() const
+{
+  string prefix = dirpath;
+  if(prefix.back() != '/') {
+    prefix += '/';
+  }
+
+  vector<string> names;
+  names.reserve(entnames.size());
+  for(const string &name : entnames) {
+    names.emplace_back(prefix + name);
+  }
+  return names;
+}
