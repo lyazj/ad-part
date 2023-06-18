3978cf6762ea3372e92e8ab8c82c0835a4be37bc
lyazj <seeson@pku.edu.cn>
2023-06-18 14:43:21 +0800

    Upgrade flatten: multiple input files supported.

--- src/flatten.cpp
+++ src/flatten.cpp
@@ -15,28 +15,17 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-  // Force line buffer output streams.
+  // Force line-buffered output streams.
   setlbf();
 
-  if(argc != 3) {
-    fprintf(stderr, "usage: %s <rootfile> <dumpfile>\n",
+  if(argc < 3) {
+    fprintf(stderr, "usage: %s <rootfile> [ <rootfile> ... ] <dumpfile>\n",
         get_invoc_short_name());
     return 1;
   }
-  const char *rootfile = argv[1];  // rootfile to read
-  const char *dumpfile = argv[2];  // dumpfile to write
-  ADPDGQuerier pdg;  // pdgid -> type info
-
-  // NOTE: Do this before loading rootfile.
-  setenv_delphes();
-
-  // Open rootfile and get Delphes tree.
-  auto file = make_shared<TFile>(rootfile, "read");
-  auto delphes = (TTree *)file->Get("Delphes");
-  Long64_t n = delphes->GetEntries();
-  printf("%s: %llu events\n", rootfile, (unsigned long long)n);
 
   // Open dumpfile to write.
+  const char *dumpfile = argv[argc - 1];  // dumpfile to write
   gzFile dump = gzopen(dumpfile, "wb");
   if(dump == NULL) {
     fprintf(stderr, "ERROR: error opening dumpfile\n");
@@ -44,21 +33,36 @@ int main(int argc, char *argv[])
   }
   shared_ptr<gzFile_s> dump_guard(dump, [](gzFile f) { gzclose(f); } );
 
-  // Set up branches.
-  auto brjet = get_branch(delphes, JET_BRANCH);
+  ADPDGQuerier pdg;  // pdgid -> type info
 
-  // Traverse entries.
-  for(Long64_t i = 0; i < n; ++i) {
-    // Fetch data.
-    delphes->GetEntry(i);
+  // NOTE: Do this before loading rootfile.
+  setenv_delphes();
 
-    // Parse and dump data.
-    size_t njet = brjet.size();
-    for(size_t j = 0; j < njet; ++j) {
-      try {
-        ADJet jet(pdg, *brjet[j]);
-        jet.write(dump);
-      } catch(const ADInvalidJet &) { }
+  // Traverse input rootfiles.
+  for(int a = 1; a < argc - 1; ++a) {
+    // Open rootfile and get Delphes tree.
+    const char *rootfile = argv[a];  // rootfile to read
+    auto file = make_shared<TFile>(rootfile, "read");
+    auto delphes = (TTree *)file->Get("Delphes");
+    Long64_t n = delphes->GetEntries();
+    printf("%s: %llu events\n", rootfile, (unsigned long long)n);
+
+    // Set up branches.
+    auto brjet = get_branch(delphes, JET_BRANCH);
+
+    // Traverse entries.
+    for(Long64_t i = 0; i < n; ++i) {
+      // Fetch data.
+      delphes->GetEntry(i);
+
+      // Parse and dump data.
+      size_t njet = brjet.size();
+      for(size_t j = 0; j < njet; ++j) {
+        try {
+          ADJet jet(pdg, *brjet[j]);
+          jet.write(dump);
+        } catch(const ADInvalidJet &) { }
+      }
     }
   }
 
