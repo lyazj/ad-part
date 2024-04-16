0a5242cf0ba3676b6e31b2a1535fc4c5730efd82
lyazj <seeson@pku.edu.cn>
2024-04-16 18:11:59 +0800

    Take off!

--- run/H2B-raw/train.sh
+++ run/H2B-raw/train.sh
@@ -5,7 +5,6 @@ python3 $HOME/work/weaver-core-dev/weaver/train.py \
     --batch-size 512 --start-lr 1e-3 --num-epochs 50 --optimizer ranger --fetch-step 0.01 \
     --gpus 2 --fetch-step 1 --in-memory \
     --samples-per-epoch $((512 * 1500)) --samples-per-epoch-val $((512 * 375)) \
-    --data-train ../sm/data4/*/*.root --data-config input.yaml \
-    --network-config example_ParticleTransformer2023EvtClassifier.py \
-    --model-prefix net \
-    --log-file train.log --tensorboard _raw
+    --data-train ../sm/data4/QCD/*.root ../sm/data4/VJets/*.root ../sm/data4/TTbar/*.root ../sm/data4/H2B/*.root \
+    --data-config input.yaml --network-config example_ParticleTransformer2023EvtClassifier.py \
+    --model-prefix raw --tensorboard _raw --log-file train.log
