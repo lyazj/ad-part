6d0cd42427b1be881227fdf04e8f8cadf6169354
lyazj <seeson@pku.edu.cn>
2024-04-28 23:08:21 +0800

    Update training and predicting scripts.

--- run/H2B-raw/pred.sh
+++ run/H2B-raw/pred.sh
@@ -7,4 +7,5 @@ python3 $HOME/work/weaver-core-dev/weaver/train.py --predict \
     --data-test ../sm/data4/QCD/*.root ../sm/data4/VJets/*.root ../sm/data4/TTbar/*.root ../sm/data4/H2B/*.root \
     --data-config input.yaml --network-config example_ParticleTransformer2023EvtClassifier.py \
     --model-prefix raw --tensorboard _raw --log-file pred.log \
-    --predict-output pred.root
+    --predict-output pred.root \
+    </dev/null 2>&1 | tee pred.sh.log
--- run/H2B-raw/train.sh
+++ run/H2B-raw/train.sh
@@ -7,4 +7,5 @@ python3 $HOME/work/weaver-core-dev/weaver/train.py \
     --samples-per-epoch $((512 * 1500)) --samples-per-epoch-val $((512 * 375)) \
     --data-train ../sm-md/data4/QCD/*.root ../sm-md/data4/VJets/*.root ../sm-md/data4/TTbar/*.root ../sm-md/data4/H2B/*.root \
     --data-config input.yaml --network-config example_ParticleTransformer2023EvtClassifier.py \
-    --model-prefix raw --tensorboard _raw --log-file train.log
+    --model-prefix raw --tensorboard _raw --log-file train.log \
+    </dev/null 2>&1 | tee train.sh.log
