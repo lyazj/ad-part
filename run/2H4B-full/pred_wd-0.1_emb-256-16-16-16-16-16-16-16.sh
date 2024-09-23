98f95d066a0763bfcc596d8cf11d9703572ab961
lyazj <seeson@pku.edu.cn>
2024-09-23 14:46:15 +0800

    Add more scripts

---
+++ run/2H4B-full/pred_wd-0.1_emb-256-16-16-16-16-16-16-16.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+NAME=$(basename $0)
+NAME=${NAME/.sh/}
+python3 $HOME/work/weaver-core-dev/weaver/train.py --predict \
+    --use-amp -o embed_dims '[256, 16], [16, 16], [16, 16], [16, 16]' -o pair_embed_dims '[16,16]' -o num_heads 4 --optimizer-option weight_decay 0.1 \
+    --batch-size 8192 \
+    --gpus 2 --fetch-step 1 --in-memory \
+    --data-test ../sm/data4/QCD/*.root ../sm/data4/VJets/*.root ../sm/data4/TTbar/*.root ../sm/data4/HH4B/*.root \
+    --data-config pred_input.yaml --network-config example_ParticleTransformer2023EvtClassifier.py \
+    --model-prefix ${NAME/pred/full} --tensorboard _pred_${NAME/pred/full} --log-file ${NAME}.log \
+    --predict-output ${NAME}.root \
+    </dev/null 2>&1 | tee ${NAME}.sh.log
---
+++ run/gghh-full/pred_wd-0.1_emb-256-16-16-16-16-16-16-16-16-16-16-16.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd ../2H4B-full
+NAME=$(basename $0)
+NAME=${NAME/.sh/}
+for ITEM in $(ls -v ../gghh/data4/); do
+    python3 $HOME/work/weaver-core-dev/weaver/train.py --predict \
+        --use-amp -o embed_dims '([256, 16, 16], [16, 16, 16], [16, 16, 16], [16, 16, 16])' -o pair_embed_dims '[16,16,16]' -o num_heads 4 --optimizer-option weight_decay 0.1 \
+        --batch-size 8192 \
+        --gpus 2 --fetch-step 1 --in-memory \
+        --data-test ../gghh/data4/${ITEM}/*.root \
+        --data-config ../gghh-full/pred_input.yaml --network-config example_ParticleTransformer2023EvtClassifier.py \
+        --model-prefix ${NAME/pred/full} --tensorboard _pred_${ITEM}_${NAME/pred/full} --log-file ../gghh-full/${NAME}_${ITEM}.log \
+        --predict-output ../gghh-full/${NAME}_${ITEM}.root \
+        </dev/null 2>&1 | tee ../gghh-full/${NAME}_${ITEM}.sh.log &
+done
+wait
